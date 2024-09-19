c9d33d7fbc4d5db2410bb037c00c9c998190d197
Eshaan Gupta <eshaan.gupta.33@gmail.com>
2024-09-19 22:54:17 +0530

    update: added backend + admin section

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import { connectDB } from "./config/db.js";
+import foodRouter from "./routes/foodRoute.js";
 
 // app config
 const app = express();
@@ -13,6 +14,10 @@ app.use(cors());
 // db connection
 connectDB();
 
+// api endpoints
+app.use("/api/food", foodRouter);
+app.use("/images", express.static("uploads"));
+
 app.get("/", (req, res) => {
   res.send("API Working");
 });
@@ -21,4 +26,4 @@ app.listen(port, () => {
   console.log(`Server started on http://localhost:${port}`);
 });
 
-// mongodb+srv://eshaangupta33:8Ava5inMGYJ2VcfT@dinedash.qjjz5.mongodb.net/?retryWrites=true&w=majority&appName=DineDash
+// mongodb+srv://eshaangupta33:8Ava5inMGYJ2VcfT@dinedash.qjjz5.mongodb.net/dinedash
