cbb3e64756fe1082de2e69bf3f80a2a494b8a4d5
Eshaan Gupta <eshaan.gupta.33@gmail.com>
2024-09-21 00:03:20 +0530

    Remove backend folder from tracking and update .gitignore

--- backend/config/db.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectDB = async () => {
-  await mongoose
-    .connect(
-      "mongodb+srv://eshaangupta33:8Ava5inMGYJ2VcfT@dinedash.qjjz5.mongodb.net/DineDash"
-    )
-    .then(() => console.log("DB Connected"));
-};
