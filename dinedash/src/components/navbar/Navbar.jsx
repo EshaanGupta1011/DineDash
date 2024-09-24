2dd1c81a5a7d933206a27d2c32819b7633533392
Eshaan Gupta <eshaan.gupta.33@gmail.com>
2024-09-25 03:26:40 +0530

    update: navbar bug

--- dinedash/src/components/navbar/Navbar.jsx
+++ dinedash/src/components/navbar/Navbar.jsx
@@ -19,7 +19,7 @@ const Navbar = ({ setShowLogin }) => {
 
   return (
     <div className="navbar">
-      <Link to="/" s>
+      <Link to="/">
         <img src={assets.logo} alt="Logo" className="logo" />
       </Link>
       <ul className="navbar-menu">
