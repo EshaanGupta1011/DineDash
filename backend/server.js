c0942bf17edf25e4ffccca518a3b3ebf4e9f033b
Eshaan Gupta <eshaan.gupta.33@gmail.com>
2024-09-25 02:58:37 +0530

    update: backend url

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,11 @@ const port = 4000;
 
 // middleware
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://eshaangupta1011.github.io/DineDash/",
+  })
+);
 
 // db connection
 connectDB();
