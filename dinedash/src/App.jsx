108bb85a7b15ad07bed1e264a02fc9d12bae4962
Eshaan Gupta <eshaan.gupta.33@gmail.com>
2024-09-25 02:29:21 +0530

    update: Importing navbar component in App.jsx

--- dinedash/src/App.jsx
+++ dinedash/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Navbar from "./components/Navbar/Navbar";
+import Navbar from "./components/Navbar/Navbar.jsx";
 import "./index.css";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
